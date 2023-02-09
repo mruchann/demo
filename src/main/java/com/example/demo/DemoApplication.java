a21e66aeb54b26af7240af3f05cfa81df7514f1f
ruchan <ruchan.yavuzdemir@icloud.com>
2023-02-09 16:09:02 +0300

    Add StudentController.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,11 +1,14 @@
 package com.example.demo;
 
+import com.example.demo.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.lang.String;
+import java.time.LocalDate;
+import java.time.Month;
 import java.util.List;
 
 @SpringBootApplication
@@ -16,7 +19,15 @@ public class DemoApplication {
 	}
 
 	@GetMapping("/query") // This annotation is used to map HTTP requests to handler methods, this is rest endpoint.
-	public List hello() { // Returns JSON array
-		return List.of("Hello", "World");
+	public List<Student> hello() { // Returns JSON array
+		return List.of( // endpoint returns a list of students
+				new Student(
+						1L,
+						"Cristiano Ronaldo",
+						"cristiano.ronaldo@getir.com",
+						LocalDate.of(1985, Month.FEBRUARY, 5),
+						36
+				)
+		);
 	}
 }
